3433c920393b81c0da29ce0f7065d8602d038cf3
Psych0delicSound <aleksey250g@gmail.com>
2025-12-15 00:54:29 +0500

    modified:   Collector/Cheliki collectori/Assets/Misc/InputSystem_Actions.inputactions 	modified:   Collector/Cheliki collectori/Assets/Prefabs/Coin.prefab 	modified:   Collector/Cheliki collectori/Assets/Scenes/Game.unity 	modified:   Collector/Cheliki collectori/Assets/Scripts/Coin.cs 	new file:   Collector/Cheliki collectori/Assets/Scripts/InputHandler.cs 	new file:   Collector/Cheliki collectori/Assets/Scripts/InputHandler.cs.meta 	modified:   Collector/Cheliki collectori/ProjectSettings/ProjectSettings.asset 	modified:   Collector/Cheliki collectori/ProjectSettings/TagManager.asset

--- Collector/Cheliki collectori/Assets/Scripts/Coin.cs
+++ Collector/Cheliki collectori/Assets/Scripts/Coin.cs
@@ -14,4 +14,5 @@ public class Coin : MonoBehaviour
 
 
 
+
 }
---
+++ Collector/Cheliki collectori/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private LayerMask layerMask;
+
+    private void Start()
+    {
+        if (cam == null) cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        HandleClick();
+    }
+
+    private void HandleClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, layerMask);
+            
+            if (hit.collider != null)
+            {
+                Debug.Log("Clicked on: " + hit.collider.name); 
+
+                if (hit.collider.CompareTag("Coin"))
+                {
+                    Coin clickedCoin = hit.collider.GetComponent<Coin>();
+
+                    Debug.Log($"+{clickedCoin.ValueOfCoin}");
+                }
+            }
+    
+        }
+    }
+}
